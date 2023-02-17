7083e32292087bad4a4f81f6a230479714a671d7
Jack7803M <jack@liviewater.com>
2023-02-17 17:51:38 -0500

    list does not need data passed

--- functions/api/host/list.ts
+++ functions/api/host/list.ts
@@ -7,16 +7,9 @@ export async function onRequestGet(context: any): Promise<Response> {
     const url = new URL(request.url);
     const roomid = url.searchParams.get("roomid");
 
-    let inData: DORequest;
-    try {
-        inData = await request.json();
-    } catch (e) {
-        return new Response("No Data Provided", { status: 400 });
-    }
-
     let id = env.STUDENTTRACKER.idFromName(roomid);
     let obj = env.STUDENTTRACKER.get(id);
-    let resp = await contactDO(obj, "/list", inData);
+    let resp = await contactDO(obj, "/list", {});
     if (resp.status !== 200) {
         console.error("Error listing room: " + await resp.text());
         return new Response("Error listing room", { status: 400 });
